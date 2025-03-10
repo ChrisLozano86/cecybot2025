614e02d31003ed69896b8f7e3007e84df1a15212
ChrisLozano86 <ChrisLozano86>
2025-03-10 11:29:02 -0600

    proyecto-base

--- index.php
+++ index.php
@@ -100,7 +100,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="imageModalLabel">Imagen</h5>
+                    <h5 class="modal-title" id="imageModalLabel">¿Estás listo para reto? </h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body text-center">
