73eb06f4915f378de1a0fcfd81c09f5515962584
ChrisLozano86 <ChrisLozano86>
2025-03-09 21:30:13 -0600

    proyecto-base

--- index.php
+++ index.php
@@ -116,9 +116,9 @@
 
         <!-- Sección Inicio -->
         <section id="inicio" class="my-5">
-          <h2>Bienvenido</h2>
+          <h2>Bases de la competencia</h2>
           <p>El CECyTE Guanajuato Unidad COMONFORT tiene la intención de crear y fomentar el interés en el amplio mundo de la robótica y la electrónica en todo el alumnado de nuestro plantel así como a los estudiantes de nuestro municipio y municipios aledaños, por lo que estamos invitándolos a participar el día <span class="font-weight-bold">21 de mayo de 2025 </span> en el Primer Torneo de Robótica y Simposium (CECyBOT Comonfort 2025) </p>
-          <p>Descarga aquí las bases del evento <a href="admin/assets/files/Bases_CECyBOT_2025.pdf" download="Bases_CECyBOT_2025.pdf"> <img src="admin/assets/img/bases.png" style="width: 100px; height: auto;"> </a></p>        
+          <a href="admin/assets/files/Bases_CECyBOT_2025.pdf" download="Bases_CECyBOT_2025.pdf"> <p class="font-weight-bold">Descarga aquí las bases del evento  <img src="admin/assets/img/bases.png" style="width: 100px; height: auto;"> </p> </a>       
         </section>
 
         <!-- Sección Categorías -->
