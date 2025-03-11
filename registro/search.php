a53c3d643e86aa312f3bc4efe7e167ef86b0f61a
ChrisLozano86 <ChrisLozano86>
2025-03-11 09:35:01 -0600

    proyecto-base

--- registro/search.php
+++ registro/search.php
@@ -73,7 +73,6 @@ $id = (isset($_REQUEST['id'])) ? $_REQUEST['id'] : null;
             <input class="form-control" type="hidden" name="url_imagen1" id="url_imagen1" value="<?php echo $registro->getUrlImagen(); ?>">
             <input class="form-control" type="hidden" name="url_imagen2" id="url_imagen2" value="<?php echo $registro->getUrlComprobante(); ?>">
             <input class="form-control" type="hidden" name="url_qr" id="url_qr" value="<?php echo $registro->getUrlQr(); ?>">
-            <input class="form-control" type="hidden" name="integrante3" id="integrante3"  value="<?php echo $registro->getIntegrante3(); ?>">
           </div>
 
             <div class="form-group">
@@ -106,6 +105,16 @@ $id = (isset($_REQUEST['id'])) ? $_REQUEST['id'] : null;
             <div class="form-group">
             <label for="integrante2">Nombre del Integrante 2</label>
             <input class="form-control" type="text" name="integrante2" id="integrante2" value="<?php echo $registro->getIntegrante2(); ?>" readonly>
+            </div>
+
+            <div class="form-group">
+            <label for="integrante3">Nombre del Integrante 3</label>
+            <input class="form-control" type="text" name="integrante3" id="integrante3" value="<?php echo $registro->getIntegrante3(); ?>" readonly>
+            </div>
+
+            <div class="form-group">
+            <label for="integrante4">Nombre del Integrante 4</label>
+            <input class="form-control" type="text" name="integrante4" id="integrante4" value="<?php echo $registro->getIntegrante4(); ?>" readonly>
             </div>
 
             <div class="form-group">
